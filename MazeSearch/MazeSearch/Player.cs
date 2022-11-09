c752d84e867dcd5b4e147d15098279766b872aec
HyunWoo Kang <kuhn0305@gmail.com>
2022-11-09 22:46:09 +0900

    AStar 알고리즘 구현 및 적용

--- MazeSearch/MazeSearch/Player.cs
+++ MazeSearch/MazeSearch/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,9 +34,11 @@ namespace MazeSearch
             Down = 2,    //2 
             Right = 3       // 4     
         }
-
-        int[] deltaY = new int[4] { -1, 0, 1, 0 };
-        int[] deltaX = new int[4] { 0, -1, 0, 1 };
+        
+        // U L D R UL DL DR UR
+        int[] deltaY = new int[] { -1, 0, 1, 0 };
+        int[] deltaX = new int[] { 0, -1, 0, 1 };
+        int[] cost = new int[] { 10, 10, 10, 10, 14, 14, 14, 14 };
 
         int _dir = (int)Dir.Up;
         List<Pos> _points = new List<Pos>();
@@ -46,8 +49,108 @@ namespace MazeSearch
             PosY = posY;
             _board = board;
 
-            //BFS();
-            Dijikstra();
+            AStar();
+        }
+
+        struct PQNode : IComparable<PQNode>
+        {
+            public int F;
+            public int G;
+            public int Y;
+            public int X;
+
+            public int CompareTo(PQNode other)
+            {
+                if (F == other.F)
+                    return 0;
+
+                return F < other.F ? 1 : -1;
+            }
+        }
+
+
+        void AStar()
+        {
+            // 점수 매기기
+            // F = G + H
+            // F = 최종 비용 (작을 수록 좋음, 경로에 따라 달라짐)
+            // G = 시작점에서 해당 좌표까지 이동하는데 드는 비용 (작을 수록 좋음, 경로에 따라 달라짐)
+            // H = 특정 좌표에서 목적지까지 얼마나 가까운지 (작을 수록 좋음, 고정)
+
+            // (y, x) 이미 방문했는지 여부 (방문 = closed 상태)
+            bool[,] closed = new bool[_board.Size, _board.Size]; // CloseList
+
+            // (y, x) 가는 길을 한 번이라도 발견 했는지
+            // 발견 x => MaxValue
+            // 발견 o => F = G + H
+            int[,] open = new int[_board.Size, _board.Size];
+
+            for (int y = 0; y < _board.Size; y++)
+            {
+                for (int x = 0; x < _board.Size; x++)
+                {
+                    open[y, x] = Int32.MaxValue;
+                }
+            }
+
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            // 오픈리스트에 있는 정보들 중에서, 가장 좋은 후보를 빠르게 뽑아오기 위한 도구
+            PriorityQueue<PQNode> priorityQueue = new PriorityQueue<PQNode>();
+
+            // 시작점 발견 (예약 진행)
+            open[PosY, PosX] = 0 + 10 * (Math.Abs(_board.DestY - PosY) + Math.Abs(_board.DestX - PosX));
+            priorityQueue.Push(new PQNode() { F = open[PosY, PosX], G = 0, Y = PosY, X = PosX });
+            parent[PosY, PosX] = new Pos(PosY, PosX);
+
+            while (true)
+            {
+                // 제일 좋은 후보를 찾는다.
+                PQNode node = priorityQueue.Pop();
+
+                // 동일한 좌표를 여러 경로로 찾아서, 더 빠른 경로로 인해 이미 방문된 경우 스킵
+                if (closed[node.Y, node.X])
+                    continue;
+
+                // 방문한다.
+                closed[node.Y, node.X] = true;
+
+                // 목적지에 도착했으면 바로 종료
+                if (node.Y == _board.DestY && node.X == _board.DestX)
+                    break;
+
+                // 상하좌우 등 이동할 수 있는 좌표인지 확인해서 예약
+                for (int i = 0; i < deltaY.Length; i++)
+                {
+                    int nextY = node.Y + deltaY[i];
+                    int nextX = node.X + deltaX[i];
+
+                    // 다음 좌표가 배열의 범위 안에 있는지 Check
+                    if (nextY < 0 || nextY >= _board.Size || nextX < 0 || nextX >= _board.Size)
+                        continue;
+                    // 다음 방문 예정 노드가 이미 방문되었는지 Check
+                    if (closed[nextY, nextX])
+                        continue;
+                    // 다음 방문 예정 노드가 벽인지 Check
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall)
+                        continue;
+
+                    // 비용계산
+                    int nextG = node.G + cost[i];
+                    int nextH = 10 * (Math.Abs(_board.DestY - nextY) + Math.Abs(_board.DestX - nextX));
+
+                    // 다른 경로에서 더 빠른 길을 이미 찾았으면 스킵
+                    if (open[nextY, nextX] < nextG + nextH)
+                        continue;
+
+                    // 예약 진행
+                    open[nextY, nextX] = nextG + nextH;
+                    priorityQueue.Push(new PQNode() { F = open[nextY, nextX], G = nextG, Y = nextY, X = nextX });
+                    parent[nextY, nextX] = new Pos(node.Y, node.X);
+                }
+            }
+
+            CalcPathFromParent(parent);
         }
 
         private void Dijikstra()
@@ -66,7 +169,7 @@ namespace MazeSearch
             Queue<Pos> queue = new Queue<Pos>();
             Pos[,] parent = new Pos[_board.Size, _board.Size];
 
-            
+
             found[PosY, PosX] = true;
             queue.Enqueue(new Pos(PosY, PosX));
             parent[PosY, PosX] = new Pos(PosY, PosX);
@@ -109,6 +212,11 @@ namespace MazeSearch
             // BFS 탐색 완료, 경로는 부모노드에 저장되어 있음
             // ======================
 
+            CalcPathFromParent(parent);
+        }
+
+        private void CalcPathFromParent(Pos[,] parent)
+        {
             // 도착지로부터 경로를 역순으로 저장하기 위함임
             int y = _board.DestY;
             int x = _board.DestX;
@@ -176,7 +284,12 @@ namespace MazeSearch
         public void Update(int deltaTick)
         {
             if (_lastIndex >= _points.Count)
-                return;
+            {
+                _lastIndex = 0;
+                _points.Clear();
+                _board.Initialize(_board.Size, this);
+                Initialize(1, 1, _board);
+            }
 
             _sumTick += deltaTick;
 
